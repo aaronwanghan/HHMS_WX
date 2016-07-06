c0f729037b9548932ba838c500c28fcef44b83b5
aaronwanghan <aaron_wh@hotmail.com>
2016-07-06 08:15:51 +0800

    07.06

--- public/javascripts/angular_app.js
+++ public/javascripts/angular_app.js
@@ -5,15 +5,30 @@ var app = angular.module("myApp",['chart.js','ngRoute'],function($httpProvider){
 });
 
 app.config(function($routeProvider,$locationProvider,ChartJsProvider){
-	$locationProvider.html5Mode({
+	/*$locationProvider.html5Mode({
 		enabled: true,
 		requireBase: false
-	});
+	});*/
 	
-	$routeProvider.when('/items/:tid/:uid',{
+	/*$routeProvider.when('/items/:tid/:uid',{
 		templateUrl: '/views/items.html',
 		controller: 'itemsController'
-	});
+	});*/
+	
+	$routeProvider.when('/login/:v',{
+		templateUrl:'/views/login.html',
+		controller: 'LoginController'
+	}).when('/tests/:v',{
+		templateUrl:'/views/tests.html',
+		controller: 'TestsListController'
+	}).when('/test/:v',{
+		templateUrl:'/views/test.html',
+		controller: 'TestController'
+	}).when('/item/:uid/:tid',{
+		templateUrl:'/views/item.html',
+		controller: 'ItemController'
+	}).otherwise({redirectTo:'/tests/:v'});
+	
 	
 	ChartJsProvider.setOptions({
 		colours: ['#CCCCCC','#CC0000','#00CCFF']
@@ -24,12 +39,12 @@ app.config(function($routeProvider,$locationProvider,ChartJsProvider){
 	});
 });
 
-app.controller("userController",function($scope,$http,$location){
-	$scope.showLoginPanel = true;
+app.controller("LoginController",function($scope,$http,$location,$routeParams){
+	//$scope.showLoginPanel = true;
 	$scope.email = "";
 	$scope.password = "";
 	$scope.login = function(){
-		var v = $location.search()['v'];
+		var v = $routeParams.v;
 		var params = {v:v,
 				email:$scope.email,
 				password:$scope.password};
@@ -41,8 +56,10 @@ app.controller("userController",function($scope,$http,$location){
 			//alert(response.data.ret);
 				if(response.data.ret == 1)
 				{
-					$scope.showLoginPanel = false;
-					$scope.message = "绑定成功！";
+					//$scope.showLoginPanel = false;
+					var path = "/tests/" + response.data.v;
+					$location.path(path);
+					//$scope.message = "绑定成功！";
 				}	
 				else
 					$scope.message = response.data.error;
@@ -51,10 +68,59 @@ app.controller("userController",function($scope,$http,$location){
 		});
 	};
 	
-	window.localName.replace('/test?v='+v);
+	//window.localName.replace('/test?v='+v);
 });
 
-app.controller("testController",function($scope,$http,$location){
+app.controller("TestsListController",function($scope,$http,$routeParams){
+	var v = $routeParams.v;
+	$http.get(SERVER_PATH+'/hhms/wx/r/nodejs/tests/'+v)
+	.success(function(data){
+		if(data.ret == 1){
+			$scope.tests = data.tests
+		}else {
+			$scope.message = data.error;
+		}
+	});
+});
+
+app.controller("TestController",function($scope,$http,$routeParams){
+	var v = $routeParams.v;
+	$http.get(SERVER_PATH+'/hhms/wx/r/nodejs/test/'+v)
+	.success(function(data){
+		if(data.ret == 1){
+			$scope.test = data.test;
+			$scope.id = data.uid;
+		}else {
+			$scope.message = data.error;
+		}
+	});
+});
+
+app.controller("ItemController",function($scope,$http,$routeParams){
+	$scope.tid = $routeParams.tid;
+	$scope.uid = $routeParams.uid;
+	
+	$scope.exit = function(){
+		window.history.go(-1);
+	};
+	
+	$http.get(SERVER_PATH+'/hhms/wx/r/nodejs/test/item/'+$scope.tid+'/'+$scope.uid)
+	.success(function(data){
+		if(data.ret == 1)
+		{
+			$scope.testitem = data.testitem;
+			$scope.chartitems = data.chartitems;
+			
+			if('D' == $scope.testitem.type)
+			{
+				//alert($scope.chartitems.length);
+				chartInit($scope);
+				$scope.showChart = true;
+			}	
+		}
+	});
+});
+/*app.controller("testController",function($scope,$http,$location){
 	$scope.showTestPanel = false;
 	var v = $location.search()['v'];
 	$http.get(SERVER_PATH+'/hhms/wx/r/nodejs/test/'+v)
@@ -107,7 +173,7 @@ function testInit(scope,data){
 	}else {
 		scope.message = data.error;
 	}
-}
+}*/
 
 function chartInit(scope){
 	var labels = new Array();
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '元向健康' });
 });
 
 router.get('/login',function(req,res,next){
 	res.render('login',{v:req.body.v});
+});
+
+router.get('/tests',function(req,res,next){
+	res.render('tests');
 });
 
 module.exports = router;
